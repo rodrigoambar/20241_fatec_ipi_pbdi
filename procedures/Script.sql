f29b94821499528f88d37c0470d15668da55ce82
Rodrigo Cordeiro Soares <digo.cordeiro04@gmail.com>
2024-06-17 15:00:34 -0300

    feat(ex11): entrega 1.4

--- procedures/Script.sql
+++ procedures/Script.sql
@@ -169,6 +169,21 @@ BEGIN
 END;
 $$;
 
+-- 1.4 Procedimento com parâmetro INOUT
+CREATE OR REPLACE PROCEDURE sp_contar_pedidos_cliente_inout(
+    INOUT p_cod_cliente INT
+) LANGUAGE plpgsql AS $$
+BEGIN
+    SELECT COUNT(*) INTO p_cod_cliente
+    FROM pedido
+    WHERE cod_cliente = p_cod_cliente;
+
+    -- Registrar log
+    INSERT INTO log_operacoes (nome_procedimento) VALUES ('sp_contar_pedidos_cliente_inout');
+END;
+$$;
+
+
 
 
 
