b35c525c2637638a5ac2cda8545b7a5986ed38c3
Rodrigo Cordeiro Soares <digo.cordeiro04@gmail.com>
2024-06-17 15:07:13 -0300

    feat (ex11): entrega 1.6

--- procedures/Script.sql
+++ procedures/Script.sql
@@ -204,7 +204,49 @@ END;
 $$;
 
 
-
+-- 1.6 Blocos anônimos para executar cada procedimento
+DO $$
+DECLARE
+    v_resultado VARCHAR(500);
+BEGIN
+    CALL sp_obter_notas_para_compor_o_troco(v_resultado, 587);
+    RAISE NOTICE 'Resultado: %', v_resultado;
+END;
+$$
+--
+DO $$
+BEGIN
+    CALL sp_contar_pedidos_cliente(1);
+END;
+$$
+---
+DO $$
+DECLARE
+    v_total_pedidos INT;
+BEGIN
+    CALL sp_contar_pedidos_cliente_out(1, v_total_pedidos);
+    RAISE NOTICE 'Total de pedidos: %', v_total_pedidos;
+END;
+$$
+---
+DO $$
+DECLARE
+    v_cod_cliente INT := 1;
+BEGIN
+    CALL sp_contar_pedidos_cliente_inout(v_cod_cliente);
+    RAISE NOTICE 'Total de pedidos: %', v_cod_cliente;
+END;
+$$
+---
+DO $$
+DECLARE
+    v_mensagem TEXT;
+BEGIN
+    CALL sp_cadastrar_clientes_variadic(v_mensagem, 'Pedro', 'Ana', 'João');
+    RAISE NOTICE '%', v_mensagem;
+END;
+$$
+------
 
 
 
