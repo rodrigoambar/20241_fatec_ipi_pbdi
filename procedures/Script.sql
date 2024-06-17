0e76ed732ceaa9dfdfb962406e8985eb3ca056d1
Rodrigo Cordeiro Soares <digo.cordeiro04@gmail.com>
2024-06-17 14:58:13 -0300

    feat(ex11): entrega 1.3

--- procedures/Script.sql
+++ procedures/Script.sql
@@ -154,6 +154,22 @@ BEGIN
 END;
 $$;
 
+-- 1.3 Procedimento para contar pedidos com variável de saída
+CREATE OR REPLACE PROCEDURE sp_contar_pedidos_cliente_out(
+    IN p_cod_cliente INT,
+    OUT p_total_pedidos INT
+) LANGUAGE plpgsql AS $$
+BEGIN
+    SELECT COUNT(*) INTO p_total_pedidos
+    FROM pedido
+    WHERE cod_cliente = p_cod_cliente;
+
+    -- Registrar log
+    INSERT INTO log_operacoes (nome_procedimento) VALUES ('sp_contar_pedidos_cliente_out');
+END;
+$$;
+
+
 
 
 
