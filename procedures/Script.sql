b034f5f89110199ec1686c545a77568c66e284a3
Rodrigo Cordeiro Soares <digo.cordeiro04@gmail.com>
2024-06-17 14:57:16 -0300

    feat(ex11): entrega 1.2

--- procedures/Script.sql
+++ procedures/Script.sql
@@ -136,6 +136,27 @@ BEGIN
 END;
 $$;
 
+--1.2 Procedimento para contar pedidos de um cliente com RAISE NOTICE
+CREATE OR REPLACE PROCEDURE sp_contar_pedidos_cliente(
+    IN p_cod_cliente INT
+) LANGUAGE plpgsql AS $$
+DECLARE
+    v_total_pedidos INT;
+BEGIN
+    SELECT COUNT(*) INTO v_total_pedidos
+    FROM pedido
+    WHERE cod_cliente = p_cod_cliente;
+
+    RAISE NOTICE 'Total de pedidos do cliente %: %', p_cod_cliente, v_total_pedidos;
+
+    -- Registrar log
+    INSERT INTO log_operacoes (nome_procedimento) VALUES ('sp_contar_pedidos_cliente');
+END;
+$$;
+
+
+
+
 
 
 
